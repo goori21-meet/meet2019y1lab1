36d8e9a02270e81184a7ccce1df679fb0cf659d4
goori21-meet <goori21@meet.mit.edu>
2019-07-21 09:34:55 -0400

    Stopping here

---
+++ meetinturtle.py
@@ -0,0 +1,26 @@
+import turtle
+turtle.left(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.right(30)
+turtle.forward(80)
+turtle.left(30)
+turtle.forward(80)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+turtle.mainloop
