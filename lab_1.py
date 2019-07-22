14c0c6bbd22034300956dcc8fa3f50afa1ccb25d
goori21-meet <goori21@meet.mit.edu>
2019-07-22 05:28:52 -0400

    Stopping here

---
+++ lab_1.py
@@ -0,0 +1,24 @@
+import turtle
+
+turtle.goto(0,100)
+turtle.goto(50,0)
+turtle.goto(50,100)
+turtle.goto(100,0)
+turtle.penup()
+turtle.goto(150,0)
+turtle.pendown()
+turtle.goto(150,100)
+turtle.goto(200,100)
+turtle.penup()
+turtle.goto(150,50)
+turtle.pendown()
+turtle.goto(200,50)
+turtle.penup()
+turtle.goto(150,0)
+turtle.pendown()
+turtle.goto(200,0)
+
+
+
+
+turtle.mainloop()
